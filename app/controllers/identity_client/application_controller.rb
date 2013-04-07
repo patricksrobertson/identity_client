3e0232dcd2d0e1e9941f4915ee481cf938fbb903
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-07 09:04:36 -0400

    Remove the methods extracted to a helper.

--- app/controllers/identity_client/application_controller.rb
+++ app/controllers/identity_client/application_controller.rb
@@ -1,11 +1,4 @@
 module IdentityClient
   class ApplicationController < ActionController::Base
-    def authenticate
-      redirect_to :login unless current_user
-    end
-
-    def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
-    end
   end
 end
