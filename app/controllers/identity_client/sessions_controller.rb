64c981898d188188510d3af95bbcf8709183adf7
Patrick Robertson <patricksrobertson@gmail.com>
2013-04-07 09:04:57 -0400

    Redirect to slash.

--- app/controllers/identity_client/sessions_controller.rb
+++ app/controllers/identity_client/sessions_controller.rb
@@ -5,7 +5,7 @@ module IdentityClient
       user = User.find_by_provider_and_uid(auth["provider"], auth["uid"].to_s) || User.create_with_omniauth(auth)
 
       session[:user_id] = user.id
-      redirect_to root_url
+      redirect_to '/'
     end
   end
 end
